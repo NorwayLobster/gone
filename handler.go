6a98ed5c9d80c7dcb68173a9177bc34ff38c3455
wangcheng08 <wangcheng08@bilibili.com>
2021-11-04 18:27:43 +0800

    add hanlder.go; add PongHandler add RankingListHandler

---
+++ handler.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func PongHandler(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+func RankingListHandler(ctx *gin.Context) {
+	rankinglistName := ctx.Param("name")
+	fmt.Printf("rankinglistName: %s\n", rankinglistName)
+	numLine, err := strconv.Atoi(ctx.Param("num"))
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+	fmt.Printf("numLine: %d\n", numLine)
+	ctx.JSON(200, gin.H{
+		"message":         "pong",
+		"rankinglistName": rankinglistName,
+		"numLine":         numLine,
+	})
+}
